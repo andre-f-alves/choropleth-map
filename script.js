f448bae42d4017530caf78d6e03f435aa9ff435a
André Alves <112015245+andre-f-alves@users.noreply.github.com>
2024-06-07 11:01:49 -0400

    setting endpoints

---
+++ script.js
@@ -0,0 +1,5 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm"
+
+const educationDataEndpoint = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json'
+
+const countyDataEndpoint = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json'
