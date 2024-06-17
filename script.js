a7595da329617f34cbbdfedf57e5cf16822d7c80
André Alves <112015245+andre-f-alves@users.noreply.github.com>
2024-06-17 11:35:29 -0300

    adding map

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7/+esm'
 
-const educationDataEndpoint = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json'
+const educationDataEndpoint =
+  'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json'
 
-const countyDataEndpoint = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json'
+const countyDataEndpoint =
+  'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json'
 
 const countyData = await d3.json(countyDataEndpoint)
 const educationData = await d3.json(educationDataEndpoint)
 
-const width = 1000
+const geojson = topojson.feature(countyData, 'counties')
+
+const width = 1200
 const height = 1000
 const padding = {
-  top: 50,
-  right: 50,
-  bottom: 50,
-  left: 50
+  top: 150,
+  right: 100,
+  bottom: 100,
+  left: 100
 }
 
 const svg = d3.select('.svg-container')
@@ -21,3 +25,13 @@ const svg = d3.select('.svg-container')
   .attr('width', width)
   .attr('height', height)
   .classed('choropleth-map', true)
+
+const map = svg.append('g')
+  .attr('transform', `translate(${padding.left}, ${padding.top})`)
+  .classed('map', true)
+
+map.selectAll('path')
+  .data(geojson.features)
+  .join('path')
+    .attr('d', d3.geoPath())
+    .classed('county', true)
